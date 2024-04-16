7b5bf95acaba7ce5631213a789bc7997d0ea03e1
why_code <119089018+VWiman@users.noreply.github.com>
2024-04-16 13:05:41 +0200

    testfix

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,14 +1,13 @@
 const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
+
 	dir: "./",
 });
 
-const config = {
-	coverageProvider: "v8",
-	testEnvironment: "jsdom",
+const customJestConfig = {
 	setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-
+	testEnvironment: "jsdom",
 };
 
-module.exports = createJestConfig(config);
+module.exports = createJestConfig(customJestConfig);
